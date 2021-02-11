8ec47f7ea07c9f49e1e59cc496b398f6d81f6c61
Aleks Kimball-Moreno <alekskimball@gmail.com>
2021-02-10 16:55:17 -0800

    updated service-worker file

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,30 +1,34 @@
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
-
-const iconSizes = ["192", "512"];
-const iconFiles = iconSizes.map(
-    (size) => `/icons/icon-${size}x${size}.png`
-);
-
-const staticFilesToPreCache = [
+const FILES_TO_CACHE = [
     "/",
+    "/db.js",
     "/index.js",
     "/index.html",
-    "/db.js",
+    "/styles.css",
     "/manifest.webmanifest",
-].concat(iconFiles);
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png"
+];
 
 
 // install
 self.addEventListener("install", function (evt) {
+    // pre cache image data
     evt.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            console.log("Your files were pre-cached successfully!");
-            return cache.addAll(staticFilesToPreCache);
-        })
+        caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
     );
 
+    // pre cache all static assets
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    );
+
+    // tell the browser to activate this service worker immediately once it
+    // has finished installing
     self.skipWaiting();
+
+
 });
 
 // activate
@@ -47,15 +51,14 @@ self.addEventListener("activate", function (evt) {
 
 // fetch
 self.addEventListener("fetch", function (evt) {
-    const { url } = evt.request;
-    if (url.includes("/all") || url.includes("/find")) {
+    if (evt.request.url.includes("/api/")) {
         evt.respondWith(
             caches.open(DATA_CACHE_NAME).then(cache => {
                 return fetch(evt.request)
                     .then(response => {
                         // If the response was good, clone it and store it in the cache.
                         if (response.status === 200) {
-                            cache.put(evt.request, response.clone());
+                            cache.put(evt.request.url, response.clone());
                         }
 
                         return response;
@@ -66,14 +69,15 @@ self.addEventListener("fetch", function (evt) {
                     });
             }).catch(err => console.log(err))
         );
-    } else {
-        // respond from static cache, request is not for /api/*
-        evt.respondWith(
-            caches.open(CACHE_NAME).then(cache => {
-                return cache.match(evt.request).then(response => {
-                    return response || fetch(evt.request);
-                });
-            })
-        );
+
+        return;
     }
+
+    evt.respondWith(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.match(evt.request).then(response => {
+                return response || fetch(evt.request);
+            });
+        })
+    );
 });
